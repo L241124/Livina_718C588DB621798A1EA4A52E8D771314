ea6f54055924c03dbd2165254e73c7a1b5c491cd
L241124 <144448546+L241124@users.noreply.github.com>
2023-09-19 10:00:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
-"""
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self._account_number = account_number
+        self._account_holder_name = account_holder_name
+        self._account_balance = initial_balance
 
-1!=1*1
-2!=2*1!--->2*1
-3!=3*2!--->3*2*1
-.
-.
-10!=10*9!--->10*9*8*.....*1
+    def deposit(self, amount):
+        if amount > 0:
+            self._account_balance += amount
+            print(f"Deposited Rs {amount}. New balance: Rs {self._account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than zero.")
 
-"""
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._account_balance:
+            self._account_balance -= amount
+            print(f"Withdrew Rs {amount}. New balance: Rs {self._account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient funds.")
 
-
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:"""
-
-1!=1*1
-2!=2*1!--->2*1
-3!=3*2!--->3*2*1
-.
-.
-10!=10*9!--->10*9*8*.....*1
-
-"""
+    def display_balance(self):
+        print(f"Account Balance for {self._account_holder_name}: Rs {self._account_balance}")
 
 
-def fact_rec(n):
-  if n == 0 or n == 1:
-    return 1
-  else:
-    return n * fact_rec(n=1)
+# Test the BankAccount class
+if __name__ == "__main__":
+    # Create an instance of BankAccount
+    my_account = BankAccount("123456789", "John", 1000.0)
 
-
-number = 2
-res = fact_rec(number)
-
-print("The factorial of {} is {}".format(number, res))
+    # Test deposit and withdrawal
+    my_account.display_balance()
+    my_account.deposit(500.0)
+    my_account.withdraw(200.0)
+    my_account.display_balance()
